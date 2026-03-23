7bd3176ec6b1d1d5c8ce50743046198e75d57143
alokMIPL <alok@madhulata.com>
2026-03-23 11:07:15 +0530

    code added

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
+const math = require("./math")
+
+console.log(math)
+// console.log(math(2, 4))
+// console.log(add(2, 5))
 console.log("Hello Node.js")
---
+++ math.js
@@ -0,0 +1,15 @@
+function add(a, b){
+  return a + b;
+}
+
+function sub(a, b){
+  return a - b;
+}
+
+// module.exports = "Testing";
+
+
+// module.exports = add;
+// module.exports = sub;
+
+module.exports = { add , sub }
