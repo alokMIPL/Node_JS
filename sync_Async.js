7bf73abebfb509f8f2f64ac84bb9895ea5018db4
alokMIPL <alok@madhulata.com>
2026-03-24 14:00:16 +0530

    code added

--- sync_Async.js
+++ sync_Async.js
@@ -1,7 +1,31 @@
-const fs = require("fs")
+const fs = require("fs");
+const os = require("os")
 
 // Blocking...
-console.log(1)
+console.log(1);
 const result = fs.readFileSync("contact.txt", "utf-8");
-console.log(result)
-console.log(2)
+console.log(result);
+console.log(2);
+
+// Blockong OR Sync OutPut like this
+
+// 1
+// Alok = +91 9958398488
+// John = +91 9958398422
+// 2
+
+// Non-Blocking...
+console.log(1);
+fs.readFile("contact.txt", "utf-8", (err, results) => {
+  console.log(results);
+});
+console.log(2);
+
+// Non_Blocking OR Async OutPut like this
+// 1
+// 2
+// Alok = +91 9958398488
+// John = +91 9958398422
+
+
+console.log(os.cpus().length);
