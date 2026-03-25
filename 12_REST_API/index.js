4ecc62b420ef629e98294763f6c2506cf11a7f07
alokMIPL <alok@madhulata.com>
2026-03-25 18:14:39 +0530

    code added

--- 12_REST_API/index.js
+++ 12_REST_API/index.js
@@ -18,6 +18,32 @@ app.get("/users", (req, res) => {
   res.send(html)
 })
 
+// Dynamic path parameters
+// GET /api/users/:id
+//  :id => varaible
+
+app.get("/api/users/:id", (req,res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user)
+})
+
+app.post("/api/users", (req,res) => {
+  // TODO: Create new user
+  return res.json({ status: "pending" })
+})
+
+
+app.patch("/api/users/:id", (req,res) => {
+  // TODO: edit new user
+  return res.json({ status: "pending" })
+})
+
+app.delete("/api/users/:id", (req,res) => {
+  // TODO: delete new user
+  return res.json({ status: "pending" })
+})
+
 app.listen(PORT, () => {
   console.log(`Server Started at Port ${PORT}`)
 })
