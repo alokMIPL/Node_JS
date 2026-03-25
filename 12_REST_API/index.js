cb7925bc2a3ab49ee680c23d621659af2863f85b
alokMIPL <alok@madhulata.com>
2026-03-25 17:23:06 +0530

    code added

--- 12_REST_API/index.js
+++ 12_REST_API/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
 // Routes
+app.get("/api/users", (req, res) => {
+  return res.json(users)
+})
+
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+      ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+  `;
+  res.send(html)
+})
 
 app.listen(PORT, () => {
   console.log(`Server Started at Port ${PORT}`)
