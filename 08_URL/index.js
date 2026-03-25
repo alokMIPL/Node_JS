a144d7f2216b5e4bc89c05d803f916334cfa179c
alokMIPL <alok@madhulata.com>
2026-03-25 06:17:13 +0530

    code added

--- 08_URL/index.js
+++ 08_URL/index.js
@@ -5,13 +5,16 @@ const url = require("url");
 // We can send different response for different url visit using switch case.
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()}: ${req.url} New Req received\n`;
-  const myUrl = url.parse(req.url);
+  const myUrl = url.parse(req.url, true);
   console.log(myUrl)
   fs.appendFile("./log.txt", log, (err, data) => {
     switch(myUrl.pathname) {
       case "/" : res.end("Home page");
       break;
-      case "/about" : res.end("About page");
+      case "/about" :
+        const username = myUrl.query.username
+        res.end(`Hi ${username}`);
+        // res.end("About page");
       break;
       case "/contact" : res.end("Contact page");
       break;
